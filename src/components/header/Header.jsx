80253915f980f0c9868a95c65964cfc1b4c8744a
yaswanthmyneni <79623546+yaswanthmyneni@users.noreply.github.com>
2022-05-05 09:06:56 +0530

    refactor: changed state variable name

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 import { useState } from "react";
 
 const Header = () => {
-  const [isDisplay, setIsDisplay] = useState(false);
+  const [isShowUserOptions, setIsShowUserOptions] = useState(false);
 
   return (
     <header className="sticky top-0 flex items-center relative w-full p-4 bg-green-600 text-slate-50">
@@ -12,21 +12,21 @@ const Header = () => {
       </NavLink>
       <CgProfile
         className="text-3xl ml-auto cursor-pointer"
-        onClick={() => setIsDisplay(!isDisplay)}
+        onClick={() => setIsShowUserOptions(!isShowUserOptions)}
       />
-      {isDisplay && (
+      {isShowUserOptions && (
         <div className="text-black text-lg cursor-pointer bg-slate-100 absolute top-11 right-8 border-2 border-gray-400 ">
           <NavLink
             to="/profile"
             className="py-1 px-10 hover:bg-lime-400 block border-b-2 border-gray-400 text-center"
-            onClick={() => setIsDisplay(!isDisplay)}
+            onClick={() => setIsShowUserOptions(!isShowUserOptions)}
           >
             Profile
           </NavLink>
           <NavLink
             to="/signin"
             className="py-1 px-8 hover:bg-lime-400 block text-center"
-            onClick={() => setIsDisplay(!isDisplay)}
+            onClick={() => setIsShowUserOptions(!isShowUserOptions)}
           >
             Login
           </NavLink>
