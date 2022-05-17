d84e6348f9130148282cb3fe9472285e14a8154e
yaswanthmyneni <79623546+yaswanthmyneni@users.noreply.github.com>
2022-05-17 23:32:42 +0530

    refactor: removed Fragment

--- src/pages/authentication-pages/signup-page/SignUpPage.jsx
+++ src/pages/authentication-pages/signup-page/SignUpPage.jsx
@@ -40,52 +40,50 @@ const SignUpPage = () => {
   };
 
   return (
-    <>
-      <main className="pt-8 pb-40">
-        <div className="w-96 py-4 px-8 m-auto border-2 rounded border-green-300">
-          <h2 className="text-3xl font-medium text-center">SignUp</h2>
-          <form className="mt-4 flex flex-col">
-            {inputFields.map((field, index) => (
-              <Fragment key={index}>
-                <label htmlFor={field}>{field}:</label>
-                <input
-                  className="p-1 mb-2 outline-none border border-gray-400"
-                  type={index >= 3 ? "password" : "text"}
-                  id={field}
-                  name={field}
-                  value={userDetails[field]}
-                  required
-                  onChange={(e) => {
-                    if (error) {
-                      setError("");
-                    }
-                    setUserDetails((prev) => ({
-                      ...prev,
-                      [field]: e.target.value,
-                    }));
-                  }}
-                />
-              </Fragment>
-            ))}
-            <p className="text-red-600">{error}</p>
-            <button
-              className="px-12 py-1 mt-4 bg-green-600 hover:bg-green-800 text-gray-50"
-              onClick={(e) => handleSignUp(e, userDetails, location)}
-            >
-              SignUp
-            </button>
-          </form>
-          <p
-            className="text-lg cursor-pointer mt-4 flex flex-wrap items-center justify-center"
-            onClick={() =>
-              navigate("/signin", { state: { from: { pathname: "/" } } })
-            }
+    <main className="pt-8 pb-40">
+      <div className="w-96 py-4 px-8 m-auto border-2 rounded border-green-300">
+        <h2 className="text-3xl font-medium text-center">SignUp</h2>
+        <form className="mt-4 flex flex-col">
+          {inputFields.map((field, index) => (
+            <Fragment key={index}>
+              <label htmlFor={field}>{field}:</label>
+              <input
+                className="p-1 mb-2 outline-none border border-gray-400"
+                type={index >= 3 ? "password" : "text"}
+                id={field}
+                name={field}
+                value={userDetails[field]}
+                required
+                onChange={(e) => {
+                  if (error) {
+                    setError("");
+                  }
+                  setUserDetails((prev) => ({
+                    ...prev,
+                    [field]: e.target.value,
+                  }));
+                }}
+              />
+            </Fragment>
+          ))}
+          <p className="text-red-600">{error}</p>
+          <button
+            className="px-12 py-1 mt-4 bg-green-600 hover:bg-green-800 text-gray-50"
+            onClick={(e) => handleSignUp(e, userDetails, location)}
           >
-            Already have an account? <FaAngleRight />
-          </p>
-        </div>
-      </main>
-    </>
+            SignUp
+          </button>
+        </form>
+        <p
+          className="text-lg cursor-pointer mt-4 flex flex-wrap items-center justify-center"
+          onClick={() =>
+            navigate("/signin", { state: { from: { pathname: "/" } } })
+          }
+        >
+          Already have an account? <FaAngleRight />
+        </p>
+      </div>
+    </main>
   );
 };
 
