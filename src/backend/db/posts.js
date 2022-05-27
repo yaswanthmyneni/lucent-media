af281fe0c05b3bea41997a9b3f4892ebe0596d68
yaswanthmyneni <79623546+yaswanthmyneni@users.noreply.github.com>
2022-05-27 20:46:02 +0530

    fix: changed username in comments

--- src/backend/db/posts.js
+++ src/backend/db/posts.js
@@ -62,7 +62,7 @@ export const posts = [
       },
       {
         _id: uuid(),
-        username: "adarshbalika",
+        username: "nandhini",
         text: "Wow! Lets make it happen.",
         votes: {
           upvotedBy: [],
@@ -84,7 +84,7 @@ export const posts = [
     comments: [
       {
         _id: uuid(),
-        username: "adarshbalika",
+        username: "nandhini",
         text: "Interesting",
         votes: {
           upvotedBy: [],
